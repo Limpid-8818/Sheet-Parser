871e390bf21a451e73315ac5366abc7d13dfa1ed
limpid8818 <2388874604@qq.com>
2025-07-16 10:04:35 +0800

    add example

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def main():
     parser = SheetParser()
 
     try:
-        html = parser.parse_file('test1.xlsx', 'test1.html', title='示例表格')
+        html = parser.parse_file('examples/basic_example.xlsx', 'examples/example.html', title='示例表格')
         print("Excel文件解析完成")
     except Exception as e:
         print(f"解析Excel文件时出错: {e}")
 
     try:
-        html = parser.parse_file('test.csv', 'testcsv.html', title='CSV示例表格')
+        html = parser.parse_file('examples/example_csv.csv', 'examples/example_csv.html', title='CSV示例表格')
         print("CSV文件解析完成")
     except Exception as e:
         print(f"解析CSV文件时出错: {e}")
